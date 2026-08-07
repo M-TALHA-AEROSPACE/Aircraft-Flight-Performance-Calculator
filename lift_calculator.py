67aa914a1f0a710f8dd7bc2f1e15540fccb3725a
MUHAMMAD TALHA <mtalhatalha567@gmail.com>
2026-08-07 16:03:27 +0500

    Update lift_calculator.py

--- lift_calculator.py
+++ lift_calculator.py
@@ -1,3 +1,12 @@
+#variables and constant:
+
+P0 = 101325      
+T0 = 288.15      
+L = 0.0065       
+g = 9.80665    
+R = 287.05
+H=altitude
+
 #function
 
 def lift_force(air_density,velocity,wing_area,lift_coefficient):
@@ -7,15 +16,26 @@ def lift_force(air_density,velocity,wing_area,lift_coefficient):
 #user input
 
 velocity=float(input("ENTER VELOCITY:"))
-air_density=float(input("ENTER AIR DENSITY:"))
-wing_area=float(input("ENTER WING AREA:"))
+altitude=float(input("ENTER ALTITUDE:"))
+lenght=float(input("ENTER LENGHT:"))
 lift_coefficient=float(input("ENTER LIFT COEFFICIENT:"))
 aircraft_weight=float(input("ENTER AIRCRAFT WEIGHT:"))
 
 total_lift= lift_force(air_density,velocity,wing_area,lift_coefficient)
+#calculations:
+
+T=T0-(L.H)
+P=P0*1-((L*H)/T0)
+p=P/R*T
+Tc=T-273.15
 
 #printing
+
 print("LIFT:",round(total_lift,2),"N")
+print("TEMPRATURE:",round(Tc),"°C")
+print("PRESSURE:",round(P),"Pa")
+print("DENSITY:",round(p),"KGM³")
+
 #conditions
 
 if total_lift>= aircraft_weight:
